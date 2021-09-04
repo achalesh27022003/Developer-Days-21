f8fe0b727304f211bdde301866d5f3d9c787517b
Achalesh <code.achalesh@gmail.com>
2021-09-04 17:12:17 +0530

    Voice Input Added

--- Web_Dev/Challenge 1/Sanganak/main.js
+++ Web_Dev/Challenge 1/Sanganak/main.js
@@ -94,3 +94,41 @@ checkbox.addEventListener('change',function(){
         document.documentElement.setAttribute('data-theme','light');
     }
 })
+
+var microphone = document.getElementById('microphone');
+microphone.onclick=function(){
+	microphone.classList.add("record");
+	var recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
+	recognition.lang = 'en-US';
+	recognition.start();
+	operations = {"plus":"+",
+				 "minus":"-",
+				 "multiply":"*",
+				 "multiplied":"*",
+				 "divide":"/",
+				 "divided":"/",
+				 "reminder":"%"}
+	
+	recognition.onresult = function(event){
+		var input = event.results[0][0].transcript;
+		for(property in operations){
+			input= input.replace(property, operations[property]);
+		}
+		document.getElementById("output-value").innerText = input;
+		setTimeout(function(){
+			evaluate(input);
+		},5000);
+		microphone.classList.remove("record");
+	}
+	
+}
+function evaluate(input){
+	try{
+		var result = eval(input);
+		document.getElementById("output-value").innerText = result;
+	}
+	catch(e){
+		console.log(e);
+		document.getElementById("output-value").innerText = "";
+	}
+}
